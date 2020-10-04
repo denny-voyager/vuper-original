5717a32b17581f340295a8d7f6441ec7f160ab91
Volodya <hotsru12@gmail.com>
2020-10-03 22:14:47 -0700

    03.10.20 22:00

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,10 @@ function myFunction() {
 
 function swapDiv(event,elem){
   elem.parentNode.insertBefore(elem,elem.parentNode.firstChild);
-}
+};
+
+$("body").on('click', '[href*="#"]', function(e){
+  var fixed_offset = 1;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
